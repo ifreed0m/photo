82a2d55b8301761da6a573526a4e400ec9255c4d
mademeng <mdmfreedom@163.com>
2017-12-05 12:37:20 +0800

    commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+package com.mademeng.test;
+
+/**
+ * @author：mademeng
+ * @date: 2017/12/5
+ * @time: 12:33
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("666666666666");
+    }
+}
